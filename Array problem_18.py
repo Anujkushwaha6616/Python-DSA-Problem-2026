b8bb665d82dde079250fae9a275b1a9e4f999784
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-09 19:35:53 +0530

    Add files via upload

---
+++ Array problem_18.py
@@ -0,0 +1,34 @@
+def rearrange_alt(arr):
+    pos = []
+    neg = []
+
+    for x in arr:
+        if x >= 0:
+            pos.append(x)
+        else:
+            neg.append(x)
+
+    res = []
+    i = j = 0
+    turn_pos = True  # start with positive
+
+    while i < len(pos) or j < len(neg):
+        if turn_pos and i < len(pos):
+            res.append(pos[i])
+            i += 1
+        elif not turn_pos and j < len(neg):
+            res.append(neg[j])
+            j += 1
+        elif i < len(pos):
+            res.append(pos[i])
+            i += 1
+        else:
+            res.append(neg[j])
+            j += 1
+
+        turn_pos = not turn_pos
+
+    return res
+
+arr = [3, 1, -2, -5, 8, -9]
+print(rearrange_alt(arr))
